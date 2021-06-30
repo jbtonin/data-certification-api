c35135becff0f5bdd729d05c6d1dd073d7b3a46f
jean brice tonin <jeanbricetonin@gmail.com>
2021-06-30 15:18:44 +0200

    mauvais coup de nous mettre une api qu on a quasiement pas vu du bootcamp

--- api/app.py
+++ api/app.py
@@ -1,13 +1,24 @@
 
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
-
 
 # define a root `/` endpoint
 @app.get("/")
 def index():
-    return {"ok": True}
+    return {"hello jb": True}
 
 
 # Implement a /predict endpoint
+@app.get("/predict")
+def predict():
+    url = 'http://localhost:8000/predict'
+    params = {"artist": "John Hartford",
+              "name": "Back in the Goodle Days",
+              "popularity": 22}
+    
+    response = requests.get(url, params=params)
+    response.json()
+    
+    return response.json()
