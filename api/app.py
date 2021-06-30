61155bd64ed70d2d85021725c63f07a68ad391be
jean brice tonin <jeanbricetonin@gmail.com>
2021-06-30 16:50:19 +0200

    api

--- api/app.py
+++ api/app.py
@@ -1,24 +1,40 @@
 
 from fastapi import FastAPI
-import requests
+import pandas as pd
+from joblib import load
 
 app = FastAPI()
 
 # define a root `/` endpoint
 @app.get("/")
 def index():
-    return {"hello jb": True}
+    return {"ok": True}
 
 
 # Implement a /predict endpoint
 @app.get("/predict")
-def predict():
-    url = 'http://localhost:8000/predict'
-    params = {"artist": "John Hartford",
-              "name": "Back in the Goodle Days",
-              "popularity": 22}
+def predict(acousticness,danceability,valence,duration_ms,speechiness,tempo,release_date,energy,explicit,mode,id,instrumentalness,key,liveness,loudness,artist,name):
+    X_pred = {'acousticness':[float(acousticness)],
+              'danceability':[float(danceability)],
+              'duration_ms':int(duration_ms),
+              'energy':[float(energy)],
+              'explicit':int(explicit),
+              'id':str(id),
+              'instrumentalness':[float(instrumentalness)],
+              'key':int(key),
+              'liveness':float(liveness),
+              'loudness':float(loudness),
+              'mode':int(mode),
+              'name':name,
+              'release_date':release_date,
+              'speechiness':float(speechiness),
+              'tempo':float(tempo),
+              'valence':float(valence),
+              'artist':artist}
     
-    response = requests.get(url, params=params)
-    response.json()
+    X_pred_df = pd.DataFrame.from_dict(X_pred)
+    model = load('model.joblib')
+    popularity = model.predict(X_pred_df)
     
-    return response.json()
+    return {'artist': artist,
+            'name':name,'popularity':'popularity'}
